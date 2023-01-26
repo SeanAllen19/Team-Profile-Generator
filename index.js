62676d75038fc498cced631ca195824d9885e064
SeanAllen19 <saallen19.dev@gmail.com>
2023-01-26 16:08:04 -0600

    added classes, subclasses. started prompt

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const manager = require("./lib/manager");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
+const employee = require("./lib/employee");
+
+const addManager = ()=>{
+    return inquirer.prompt([
+        {
+            type:"input",
+            name:"name",
+            message:"What is the name of the manager of the team?",
+        },
+        {
+            type:"input",
+            name:"ID",
+            message:"What is the manager employee ID?",
+        },
+        {
+            type:"input",
+            name:"email",
+            message:"what is the manager email address?",
+        },
+        {
+            type:"input",
+            name:"officeNumber",
+            message:"What is the office number?",
+        },
+    ])
+    .then (managerInput=>{
+        console.log(managerInput)
+        const {name, ID, email, officeNumber} = managerInput;
+        const manager = new Manager (name, ID, email, officeNumber);
+        team.push();
+        console.log(team)
+        console.log(manager);
+    })
+}
+addManager();
